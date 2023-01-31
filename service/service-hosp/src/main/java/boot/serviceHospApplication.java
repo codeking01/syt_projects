687c51626faf3c7281447b49ec9a9afdf7eb7dde
codeking <jialiang_xiong@163.com>
2023-01-31 21:43:30 +0800

    2023.1.31,commit.完成swagger2的集成，完成Logback的配置（日志写入本地），完成医院接口的一个crud案例
    Signed-off-by: codeking <1094234526@qq.com>

--- service/service-hosp/src/main/java/boot/serviceHospApplication.java
+++
@@ -1,8 +0,0 @@
-package boot;
-
-/**
- * @author : codeking
- * @create : 2023/1/30 23:17
- */
-public class serviceHospApplication {
-}
--- service/service-hosp/src/main/java/com/codeking/boot/serviceHospApplication.java
+++ service/service-hosp/src/main/java/com/codeking/boot/serviceHospApplication.java
@@ -2,12 +2,14 @@ package com.codeking.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author : codeking
  * @create : 2023/1/30 23:13
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.codeking")
 public class serviceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(serviceHospApplication.class, args);
