582e8cba887c50c3734ee106ea8a2ee51e2d0890
codeking <jialiang_xiong@163.com>
2023-03-10 20:58:32 +0800

    2023.3.10，commit.修改文件的时候没有将cmn服务注册，加尚注册 @EnableFeignClients(basePackages = "com.codeking")
    Signed-off-by: codeking <codekingbear@gmail.com>

--- service/service-cmn/src/main/java/com/codeking/yygh/yyghCmnApplication.java
+++ service/service-cmn/src/main/java/com/codeking/yygh/yyghCmnApplication.java
@@ -3,6 +3,7 @@ package com.codeking.yygh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(value = "com.codeking")
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.codeking")
 public class yyghCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(yyghCmnApplication.class, args);
