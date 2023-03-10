c51a581529e882e2a6afc4c8edcd9eaec7247002
codeking <jialiang_xiong@163.com>
2023-03-10 16:10:54 +0800

    2023.3.10，commit.完成登陆接口，放入token,使用了JWT技术，解决了在server-gateway的冲突，发现是swagger的依赖问题，换成引入common-util了
    Signed-off-by: codeking <codekingbear@gmail.com>

---
+++ service/service-msm/src/main/java/com/codeking/yygh/msm/controller/MsmApiController.java
@@ -0,0 +1,51 @@
+package com.codeking.yygh.msm.controller;
+
+import com.codeking.yygh.common.result.Result;
+import com.codeking.yygh.common.utils.RandomUtil;
+import com.codeking.yygh.msm.service.MsmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/api/msm")
+public class MsmApiController {
+
+    @Autowired
+    private MsmService msmService;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    //发送手机验证码
+    @GetMapping("send/{phone}")
+    public Result sendCode(@PathVariable String phone) {
+        //从redis获取验证码，如果获取获取到，返回ok
+        // key 手机号  value 验证码
+        String code = redisTemplate.opsForValue().get(phone);
+        if (!StringUtils.isEmpty(code)) {
+            return Result.ok();
+        }
+        //如果从redis获取不到，
+        // 生成验证码，
+        //code = RandomUtil.getSixBitRandom();
+        // 这个地方为了测试，直接写死
+        code = "123456";
+        //调用service方法，通过整合短信服务进行发送
+        //boolean isSend = msmService.send(phone, code);
+        boolean isSend = true;
+        //生成验证码放到redis里面，设置有效时间
+        if (isSend) {
+            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
+            return Result.ok();
+        } else {
+            return Result.fail().message("发送验证码失败");
+        }
+    }
+}
